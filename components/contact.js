58f14b8b0049e62087107341470c4747187e0f27
chrisfleet1 <85644957+chrisfleet1@users.noreply.github.com>
2023-02-01 21:14:00 +0000

    testing - form submission bug

--- components/contact.js
+++ components/contact.js
@@ -7,7 +7,7 @@ export default function Contact() {
         <div className={styles.sections_header}>
             <h2 >Contact Us</h2>
         </div>
-        <form className={styles.form_container} data-netlify="true" method='post' name="contact" data-netlify-recaptcha="true">
+        <form className={styles.form_container} data-netlify="true" method='POST' name="contact" data-netlify-recaptcha="true" data-netlify-honeypot="bot-field" action="Thank you">
         <p>Book your free consultation here, e-mail us at <Link href="mailto: jack@jackcannonfitness.co.uk" className={styles.link}>jack@jackcannonfitness.co.uk</Link>!</p>
             <div className={styles.input_container}>
                 <label for="name" className={styles.hidden}>Contact Name: </label>
